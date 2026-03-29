e8d159c9ba1e229a33eead897066345ed42cb5a1
Anna Chappelle <anna.chappelle@live.longwood.edu>
2026-03-28 20:23:48 -0400

    typo

--- src/main/java/com/byrdparkgeese/hackathonbackend/data/repositories/RepliesByTypeRepository.java
+++ src/main/java/com/byrdparkgeese/hackathonbackend/data/repositories/RepliesByTypeRepository.java
@@ -1,9 +1,10 @@
 package com.byrdparkgeese.hackathonbackend.data.repositories;
 
+import com.byrdparkgeese.hackathonbackend.data.entities.RepliesByTypeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RepliesByType extends JpaRepository<RepliesByType, Long> {
+public interface RepliesByTypeRepository extends JpaRepository<RepliesByTypeEntity, Long> {
 
 }
