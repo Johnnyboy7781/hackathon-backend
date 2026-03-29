212f487b5c3d07e75213fc2555e59deed5e2afbd
Jonathan McDonnell <jonathan.code@atomicmail.io>
2026-03-28 21:35:39 -0400

    sidewalk sidewalk potgole gpoegl

--- src/main/java/com/byrdparkgeese/hackathonbackend/controllers/TextHookController.java
+++ src/main/java/com/byrdparkgeese/hackathonbackend/controllers/TextHookController.java
@@ -73,14 +73,14 @@ public class TextHookController {
         var optionsList = List.of("12345", conversation.getAddress(), conversation.getIssueDesc());
 
         if (dbCategory.getCategory() == "Pothole On Road") {
+            optionsList.add("Pothole");
+            optionsList.add("0");
+            optionsList.add("0");
+        } else if (dbCategory.getCategory() == "Repair Sidewalk Or Ramp") {
             optionsList.add("Sidewalk");
             optionsList.add("0");
             optionsList.add("0");
             optionsList.add("ArrowDown||Enter");
-        } else if (dbCategory.getCategory() == "Repair Sidewalk Or Ramp") {
-            optionsList.add("Pothole");
-            optionsList.add("0");
-            optionsList.add("0");
         } else {
             return;
         }
