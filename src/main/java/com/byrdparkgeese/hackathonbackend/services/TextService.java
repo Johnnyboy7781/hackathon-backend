cde9897027505a2bf26c2e6436f99049c2159d72
Jonathan McDonnell <jonathan.code@atomicmail.io>
2026-03-28 22:14:25 -0400

    chore

--- src/main/java/com/byrdparkgeese/hackathonbackend/services/TextService.java
+++ src/main/java/com/byrdparkgeese/hackathonbackend/services/TextService.java
@@ -56,6 +56,9 @@ public class TextService {
        String formattedMessage = "Below are the following open reports that seem to correspond with your address and issue!\n\n";
 
         for (Report report : reportsList) {
+            if (report == null) {
+                continue;
+            }
             System.out.println("Report: %s".formatted(report.location()));
             formattedMessage += "-----\n\n";
             formattedMessage += "Title: %s\n".formatted(report.serviceName());
